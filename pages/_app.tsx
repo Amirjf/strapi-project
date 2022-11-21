aa685bfdd28498223fd94a7e5509393f96f3d963
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-11-21 12:27:31 +0330

    migrate to typescript and added header and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { MantineProvider } from '@mantine/core';
-import Layout from '../components/layout/Layout';
+import React from 'react';
+import { Layout } from '../components/layout/Layout';
+
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
