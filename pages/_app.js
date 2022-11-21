c6c61b9de31cb50c379f7aa92b139084afd7a45a
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-11-21 11:30:43 +0330

    initialize project and added shared components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { MantineProvider } from '@mantine/core';
+import Layout from '../components/layout/Layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: 'dark',
+      }}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
